c84eaac85e76be185078a7b22fa5f0f5f87a5525
k-payl <aaa@bbb.com>
2020-01-08 04:06:14 +0700

    move files

---
+++ corerender/dx12/dx12common.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "pch.h"
+#include "common.h"
+#include "icorerender.h"
+
+inline constexpr unsigned D3D12MaxRootParameters = 16;
+
+DXGI_FORMAT engineToDXGIFormat(VERTEX_BUFFER_FORMAT format);
+
--- dx12common.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "pch.h"
-#include "common.h"
-#include "icorerender.h"
-
-inline constexpr unsigned D3D12MaxRootParameters = 16;
-
-DXGI_FORMAT engineToDXGIFormat(VERTEX_BUFFER_FORMAT format);
-
